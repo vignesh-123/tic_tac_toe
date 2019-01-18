90b750172c6494729da160c5455ab67578faef9c
Deepika-R <mailfordeepika97@gmail.com>
2019-01-18 17:11:43 +0530

    package build

--- TicTacToe/src/tictactoe/Display.java
+++ TicTacToe/src/tictactoe/Display.java
@@ -1,15 +1,24 @@
 package tictactoe;
 
 public class Display {
-	public void display(char arr[])
+	public char[] display(char arr[])
 	{
+		char check[]=new char[15];
+		int ind=0;
 		for(int index=0;index<arr.length;index+=3) {
 			for(int i=index;i<index+3;i++)
 			{
-				System.out.print(arr[index]+"|");
+				System.out.print(arr[i]);
+				check[ind++]=arr[i];
+				if(i!=index+2)
+				{
+					System.out.print('|');
+					check[ind++]='|';
+				}
 			}
 			System.out.println();
 		}
+		return check;
 	}
 	
 }
