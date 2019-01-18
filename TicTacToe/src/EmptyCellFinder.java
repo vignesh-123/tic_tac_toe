529502abccf9d3e2b886f1ecb3bab9077bf58855
Bagya-lakshmi <bagyalakshmi.n98@gmail.com>
2019-01-18 17:48:56 +0530

    baghya changes2

--- TicTacToe/src/EmptyCellFinder.java
+++ TicTacToe/src/EmptyCellFinder.java
@@ -1,13 +1,16 @@
 import java.util.*;
 public class EmptyCellFinder {
 	static int flag=0;
-	public void FindingEmptyCell(char TicTacArray[]) {
+	public char[] FindingEmptyCell(char TicTacArray[],char value) {
 		char useInputVal;
+		char c=1;
 		char player1='x';
 		char player2='o';
-		System.out.println("Enter the Values");
+		
+		//System.out.println("Enter the Values");
 		Scanner sr=new Scanner(System.in);
-		useInputVal=sr.next().charAt(0);
+		//useInputVal=sr.next().charAt(0);
+		useInputVal=value;
 		for(int input=0;input<TicTacArray.length;input++) {
 			if(useInputVal==TicTacArray[input]) {
 				if(flag==0) {
@@ -18,10 +21,21 @@ public class EmptyCellFinder {
 					TicTacArray[input]=player2;
 					flag=0;
 				}
+				c=0;
+				break;
 				
 		
 			}
 			
+		}
+		if(c!=0) {
+			System.out.println("Enter the valid option");
+			useInputVal=sr.next().charAt(0);
+			return new EmptyCellFinder().FindingEmptyCell(TicTacArray,useInputVal);
+		}
+		else {
+		
+		return TicTacArray;
 		}
 		
 	}
